d2203cf946ce992ecdb5d22a429e35d4f3a29c81
Diego Saavedra <dsaavedra88@gmail.com>
2022-11-24 04:19:55 -0500

    Part 2

--- main.py
+++ main.py
@@ -17,8 +17,16 @@ def create_client(client_name):
 #2. Creamos la funcion list_clients
 def list_clients():
     global clients
-
     print(clients)
+
+
+def update_client(client_name, update_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ",", update_client_name + ",")
+    else:
+        print("El cliente no esta en la lista de clientes")
 
 #Funcion de ayuda ','
 def _add_comma():
@@ -32,18 +40,33 @@ def _print_welcome():
     print("*"* 50)
     print("¿Qué quieres hacer?")
     print("[C]rear cliente")
+    print("[A]ctualizar cliente")
     print("[E]liminar cliente")
+
+# 4 Refactoring de Cual es el nombre del cliente
+def _get_cliente_name():
+    return input("Cual es el nombre del cliente ")
+
 
 # 0 Ejecución del programa
 if __name__ == '__main__':
     _print_welcome()
+
     command = input()
+    command = command.upper()
+
     if command == "C":
-        client_name = input("Cual es el nombre del cliente: ")
+        client_name = _get_cliente_name()
         create_client(client_name)
         list_clients()
-    elif command == "D":
+    elif command == "E":
         pass
+    elif command == "A":
+        client_name = _get_cliente_name()
+        update_client_name = input("Cual es el el nuevo nombre del cliente: ")
+        update_client(client_name, update_client_name)
+        list_clients()
+
     else:
         print("Comando invalido")
 
